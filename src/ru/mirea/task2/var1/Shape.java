84eec689e096db212372182362f391b7fb54ae1f
Svetlana Babenkova <shawol358@gmail.com>
2021-11-23 18:26:14 +0300

    added Task2 var1

---
+++ src/ru/mirea/task2/var1/Shape.java
@@ -0,0 +1,17 @@
+package ru.mirea.task2.var1;
+
+public class Shape {
+    String name;
+
+    public Shape(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+}
---
+++ src/ru/mirea/task2/var1/TestShape.java
@@ -0,0 +1,11 @@
+package ru.mirea.task2.var1;
+
+
+public class TestShape {
+    public static void main(String[] args)
+    {
+        Shape n1=new Shape("Circle");
+        n1.toString();
+        System.out.println(n1);
+    }
+}
